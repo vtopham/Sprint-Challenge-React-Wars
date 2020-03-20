c5b309c94d8945b9cb3dfa3cc8bf89a5e733cca8
Victoria  Topham <topham.victoria@gmail.com>
2020-03-20 08:15:42 -0700

    successfully called data from the API

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import CharacterCard from "./components/CharacterDisplay";
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,22 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.co/api/people/1/")
+    .then((response) =>{
+      console.log(response);
+    })
+    .catch((response) => {
+      alert("Try again, friend-o");
+    })
+  },[])
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {/* <CharacterCard /> */}
     </div>
   );
 }
